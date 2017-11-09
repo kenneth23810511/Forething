bd9e4597b23c29ee80cbeefdc1888d2c4d7da39f
kenneth <kenneth_yao@epam.com>
2017-11-09 11:05:00 +0800

    Support Image Selection.

--- ProductView.js
+++ ProductView.js
@@ -18,6 +18,7 @@ import Common from './utils/Common';
 import FetchBack from './utils/FetchBack';
 import RNFS from 'react-native-fs';
 
+
 export default class ProductView extends Component {
     constructor() {
         super();
@@ -28,11 +29,12 @@ export default class ProductView extends Component {
                       x:10,
                       y:20,
                       documentPath: RNFS.DocumentDirectoryPath+'/',
+                      avatarSource:null,
                       internalContainerwidth: 200,
                       internalContainerheight: 200,
                       internalContainerleft: 0,
                       internalContainertop: 0,
-                      loading: false
+                      loading: false,
                     };
         let params = {'start':'0',limit:'20','isNeedCategory': true, 'lastRefreshTime': '2016-09-25 09:45:12'};
                 FetchBack.Post(this, params, function (target, set) {
@@ -81,6 +83,49 @@ export default class ProductView extends Component {
         gesturesEnabled: true
     };
 
+    upLoader() {
+            var ImagePicker = require('react-native-image-picker');
+
+            // More info on all the options is below in the README...just some common use cases shown here
+            var options = {
+              title: 'Select Avatar',
+              customButtons: [
+                {name: 'fb', title: 'Choose Photo from Facebook'},
+              ],
+              storageOptions: {
+                skipBackup: true,
+                path: 'images'
+              }
+            };
+
+            /**
+             * The first arg is the options object for customization (it can also be null or omitted for default options),
+             * The second arg is the callback which sends object: response (more info below in README)
+             */
+            ImagePicker.showImagePicker(options, (response) => {
+              console.log('Response = ', response);
+
+              if (response.didCancel) {
+                console.log('User cancelled image picker');
+              }
+              else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+              }
+              else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+              }
+              else {
+                let source = { uri: response.uri };
+
+                // You can also display the image using data:
+                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                this.setState({
+                  avatarSource: source
+                });
+              }
+            });
+        }
 
   render() {
     var localpath =  Common.GetLocalFullPath(this.props.navigation.state.params.ListViewClickItemHolder.Icon);
@@ -90,6 +135,8 @@ export default class ProductView extends Component {
             <View>
                 <Image source={{uri:localpath}} style={this.internalImageStyle()} />
                      <Text>{ this.props.navigation.state.params.ListViewClickItemHolder.FirstName }</Text>
+                     <Image source={this.state.avatarSource} style={styles.uploadAvatar} />
+                     <Button onPress={this.upLoader.bind(this)} title='Load'></Button>
               </View>
           </View>
       </View>
@@ -106,6 +153,10 @@ const styles = StyleSheet.create({
     width:'100%',
     height:'100%',
   },
+  uploadAvatar: {
+      width:160,
+      height:160,
+    },
     styleproducts:{
         width:'90%',
     },
